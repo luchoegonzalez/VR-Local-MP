0dc9721793ecee6910f58512bc3c4fdb9d6777d2
luchoegonzalez <legonzalez@dc.uba.ar>
2025-07-31 16:14:02 -0300

    fixed ActionBased warnings + delete unused assets

--- Assets/VRMPAssets/Scripts/ActivateXRControllers.cs
+++ Assets/VRMPAssets/Scripts/ActivateXRControllers.cs
@@ -1,29 +1,31 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using Unity.Netcode;
-using UnityEngine.XR.Interaction.Toolkit;
 
-public class ActivateXRControllers : NetworkBehaviour {
+public class ActivateXRControllers : NetworkBehaviour
+{
 
     [SerializeField]
-    private ActionBasedController[] XRControllers;
+    private GameObject[] XRControllers;
 
-    public override void OnNetworkSpawn() {
+    public override void OnNetworkSpawn()
+    {
         ActivateControllers();
         base.OnNetworkSpawn();
     }
 
-    public void ActivateControllers() {
-        for (int i = 0; i < XRControllers.Length; i++) {
-            XRControllers[i].gameObject.SetActive(true);
+    public void ActivateControllers()
+    {
+        foreach (var controller in XRControllers)
+        {
+            controller.SetActive(true);
         }
     }
 
-    public void DeactivateControllers() {
-        for (int i = 0; i < XRControllers.Length; i++) {
-            XRControllers[i].gameObject.SetActive(false);
+    public void DeactivateControllers()
+    {
+        foreach (var controller in XRControllers)
+        {
+            controller.SetActive(false);
         }
     }
-
 }
